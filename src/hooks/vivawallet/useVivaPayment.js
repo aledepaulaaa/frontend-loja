53493b3b1be56852010434010904d095530f7bc9
aledepaulaaa <contatoaledepaula@gmail.com>
2025-01-07 15:16:23 -0300

    Ajuste na URL checkout em useVivaPayment.js

--- src/hooks/vivawallet/useVivaPayment.js
+++ src/hooks/vivawallet/useVivaPayment.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 
 const urlDevelopment = process.env.DEV_URL_PAYMENT
 const urlProduction = process.env.NEXT_PUBLIC_PRODUCTION_URL_PAYMENT
-const urlCheckout = process.env.URL_CHECKOUT
 
 export default function usePaymentVivaWallet() {
     const [error, setError] = React.useState(null)
@@ -12,14 +11,14 @@ export default function usePaymentVivaWallet() {
     // Função para gerar o token no servidor e gerar o pagamento
     const useVivaPayment = async (paymentData) => {
         try {
-            const payment = await axios.post(urlProduction, paymentData)
+            const payment = await axios.post(vivaWalletURL, paymentData)
             const response = payment.data
             setPaymentData(response)
 
             console.log("Resposta do backend: ", response.data)
             // Redirecionar o usuário para o checkout da Viva Wallet
             if (response.orderCode) {
-                window.location.href = `${urlCheckout}${response.orderCode}`;
+                window.location.href = `https://demo.vivapayments.com/web/checkout?ref=${response.orderCode}`;
             } else {
                 throw new Error("Código do pedido não retornado.");
             }
